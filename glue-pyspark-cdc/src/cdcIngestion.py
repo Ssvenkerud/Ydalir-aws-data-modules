4e71fd8071150e44ae76fe6b3efbb1b49dd68d10
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-04-10 07:06:30 +0200

    Adding initial source highwater mark

--- glue-pyspark-cdc/src/cdcIngestion.py
+++ glue-pyspark-cdc/src/cdcIngestion.py
@@ -1,2 +1,84 @@
+import pyspark.sql.functions as F
+
+
+# change so that each method recives all its input as parameters.
 class cdcIngestion:
-    pass
+    def __init__(
+        self,
+        sparkSession,
+        source_data_location="",
+        high_water_column="",
+        source_data=None,
+        target_data=None,
+    ):
+        self.spark = sparkSession
+        self.source_data_location = source_data_location
+        self.high_water_column = high_water_column
+        self.source_high_water_mark = None
+        self.target_data = target_data
+        self.source_data = source_data
+
+    def get_source_high_watermark(
+        self,
+        spark=None,
+        high_water_column=None,
+        source_data=None,
+    ):
+        if spark is None:
+            spark = self.spark
+        if high_water_column is None:
+            high_water_column = self.high_water_column
+        if source_data is None:
+            source_data = self.source_data
+
+        if source_data is not None:
+            self.source_high_water_mark = (
+                source_data.select(high_water_column)
+                .agg(F.max(high_water_column))
+                .collect()[0][0]
+            )
+        return self.source_high_water_mark
+
+
+    def get_target_high_watermark(
+            self,
+            spark=None,
+            high_water_column=None,
+            target_data=None,
+            ):
+        if spark is None:
+            spark = self.spark
+        if  high_water_column in None:
+            high_water_column = self.high_water_column
+        if target_data is None:
+            target_data = self.target_data
+
+
+    def get_high_water_mark(
+        self,
+        spark=None,
+        source_data=None,
+        high_water_mark=None,
+        high_water_column=None,
+        target_data=None,
+    ):
+        if spark is None:
+            spark = self.spark
+        if source_data is None:
+            source_data = self.source_data
+        if high_water_column is None:
+            high_water_column = self.high_water_column
+
+        if source_data is not None:
+            self.source_high_water_mark = (
+                source_data.select(high_water_column)
+                .agg(F.max(high_water_column))
+                .collect()[0][0]
+            )
+        else:
+            pass
+
+        return self.source_high_water_mark, self.target_high_water_mark
+
+    def run(self):
+        self.get_high_water_mark()
--- glue-pyspark-cdc/tests/test_pyspark_cdc.py
+++ glue-pyspark-cdc/tests/test_pyspark_cdc.py
@@ -9,84 +9,6 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 from src.cdcIngestion import cdcIngestion
 
-TestSpark = SparkSession.builder.getOrCreate()
-
-test_cdc_log_schema = StructType(
-    [
-        StructField("Identificator", StringType(), False),
-        StructField("stringvar", StringType(), True),
-        StructField("numvar", LongType(), True),
-        StructField("Boolvar", BooleanType(), True),
-        StructField("timevar", StringType(), True),
-        StructField("datevar", StringType(), True),
-        StructField("op", StringType(), True),
-        StructField("transact_id", StringType(), False),
-    ]
-)
-
-test_cdc_log_valid_data = [
-    ("id1", "monkey", 1, True, "2021-01-01 01:01:01.000", "2021-01-01", "", ""),
-    ("id2", "tiger", 1345, False, "2021-02-02 02:02:02.000", "2022-02-02", "", ""),
-    ("id3", "Elefant", 256, False, "2021-03-03 03:03:03.000", "2023-03-03", "", ""),
-    ("id4", "snake", 10854751, True, "2021-04-04-04:04:04.000", "2021-04-04", "", ""),
-    ("id5", "moose", 17438531, True, "2021-05-05-05:05:05.000", "2021-05-05", "", ""),
-    ("id5", "moose", 17438531, True, "2021-06-06-06:06:06.000", "2026-06-06", "U", "1"),
-    ("id5", "moose", 17465731, True, "2021-07-07-07:07:07.000", "2027-07-07", "U", "2"),
-    (
-        "id1",
-        "monkey",
-        17438531,
-        True,
-        "2021-08-08-08:08:08.000",
-        "2028-08-08",
-        "U",
-        "3",
-    ),
-    ("id5", "moose", 17438531, True, "2021-09-09-09:09:09.000", "2029-09-09", "U", "4"),
-    (
-        "id6",
-        "Squirrel",
-        17438531,
-        True,
-        "2021-10-10-10:10:10.000",
-        "2030-10-10",
-        "I",
-        "5",
-    ),
-    ("id2", None, None, None, None, None, "D", "6"),
-]
-
-test_conformed_data = [
-    ("id3", "Elefant", 256, False, "2021-03-03 03:03:03.000", "2023-03-03", "", ""),
-    ("id4", "snake", 10854751, True, "2021-04-04-04:04:04.000", "2021-04-04", "", ""),
-    (
-        "id1",
-        "monkey",
-        17438531,
-        True,
-        "2021-08-08-08:08:08.000",
-        "2028-08-08",
-        "U",
-        "3",
-    ),
-    ("id5", "moose", 17438531, True, "2021-09-09-09:09:09.000", "2029-09-09", "U", "4"),
-    (
-        "id6",
-        "Squirrel",
-        17438531,
-        True,
-        "2021-10-10-10:10:10.000",
-        "2030-10-10",
-        "I",
-        "5",
-    ),
-]
-
-
-@pytest.fixture(scope="session")
-def spark():
-    return SparkSession.builder.appName("Test_engine").getOrCreate()
-
 
 def test_engine_test(spark):
     assert spark.active()
@@ -94,6 +16,22 @@ def test_engine_test(spark):
     print(spark_conf)
 
 
-def test_identify_highwatermark_in_source(spark):
-    input_data = spark.createDataFrame(test_cdc_log_valid_data, test_cdc_log_schema)
-    cdc = cdcIngestion(spark, input_data, high_water_mark="Identificator")
+def test_get_source_high_watermark(spark, cdc_log_valid_raw_data, cdc_log_valid_schema):
+    input_data = spark.createDataFrame(cdc_log_valid_raw_data, cdc_log_valid_schema)
+    cdc1 = cdcIngestion(spark)
+    source_high_watermark = cdc1.get_source_high_watermark(
+        source_data=input_data,
+        high_water_column="Identificator",
+    )
+    assert source_high_watermark == "id6"
+
+
+def test_get_target_high_watermark(spark, cdc_log_valid_schema, cdc_conformed_data):
+    input_data = spark.createDataFrame(cdc_conformed_data, cdc_log_valid_schema)
+    cdc = cdcIngestion(spark)
+    target_high_watermark = cdc.get_target_high_watermark(
+        spark,
+        high_water_column="identificator",
+        target_data=input_data,
+    )
+    assert target_high_water_mark == "id6"
